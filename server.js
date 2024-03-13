cca8a17af36adb0de9e79b5e77659433e5e33b43
kjcabPL <kcabrera@profoundlogic.com>
2024-03-13 16:46:45 +0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { engine } = require("express-handlebars"); // uses an constant object "{}"  now
+const url = require("url");
+const path = require("path");
+
+// server requirements
+const port = process.env.PORT || 5000; // used for herokuapp
+const app = express();
+
+//  page constants
+const otherstuff = "this is other stuff";
+
+// set handlebars as middleware for creating dynamic content
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars"); 
+
+// set up handlebar routes
+// route to homepage
+app.get('/', (req, res) => {
+    res.render("home", { 
+        stuff: otherstuff
+    });
+})
+
+// set static base folder
+app.use( express.static( path.join(__dirname, "views" )));
+
+app.listen(port, ()=> {
+    console.log("Server listening on port: " + port);
+})
